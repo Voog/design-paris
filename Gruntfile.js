86499e102ecd4af024878aa4c6e375cb89978c67
kaspar-naaber <kaspar.naaber@outlook.com>
2014-07-22 10:13:29 +0300

    Update Grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,12 +1,13 @@
 module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+
     concat: {
-      options: {
-        separator: ';'
-      },
-      dist: {
-        src: ['javascripts/src/jquery.js', 'javascripts/src/*.js'],
+      build: {
+        src: [
+        'javascripts/src/jquery.js',
+        'javascripts/src/*.js'
+        ],
         dest: 'javascripts/application.js'
       }
     },
@@ -16,7 +17,10 @@ module.exports = function(grunt) {
         files: [{
           expand: true,
           cwd: 'javascripts/',
-          src: ['*.js', '!*.min.js'],
+          src: [
+          '*.js',
+          '!*.min.js'
+          ],
           dest: 'javascripts/',
           ext: '.min.js'
         }]
@@ -24,7 +28,10 @@ module.exports = function(grunt) {
     },
 
     sass: {
-      dist: {
+      build: {
+        options: {
+          style: 'expanded'
+        },
         files: [{
           expand: true,
           cwd: 'stylesheets/sass',
@@ -36,43 +43,75 @@ module.exports = function(grunt) {
     },
 
     cssmin: {
-      minify: {
+      build: {
         expand: true,
         cwd: 'stylesheets/',
         src: ['*.css', '!*.min.css'],
         dest: 'stylesheets/',
-        ext: '.min.css'
+        ext: '.min.css',
+        options: {
+          spawn: false
+        }
       }
+    },
+
+    imagemin: {
+      images: {
+        files: [{
+          expand: true,
+          cwd: 'images/src',
+          src: ['*.{png,jpg,gif}',],
+          dest: 'images/'
+        }]
+      },
+    },
+
+    svgmin: {
+      build: {
+        files: [{
+          expand: true,
+          cwd: 'assets/src',
+          src: ['*.svg'],
+          dest: 'assets/',
+          ext: '.svg'
+        }]
+      },
     },
 
     watch: {
       concat: {
         files: ['javascripts/src/*.js'],
-        tasks: "concat"
+        tasks: 'concat'
       },
 
       uglify: {
-        files: ['javascripts/application.js', 'javascripts/application.min.js'],
-        tasks: ['uglify']
+        files: ['javascripts/*.js', '!javascripts/*.min.js'],
+        tasks: 'uglify',
+        options: {
+          spawn: false
+        }
       },
 
-      sass: {
-        files: ['stylesheets/sass/*.scss', '!stylesheets/*.css'],
-        tasks: ['sass']
-      },
-
-      cssmin: {
-        files: ['stylesheets/*.css', '!stylesheets/*.min.css'],
-        tasks: ['cssmin']
+      css: {
+        files: ['stylesheets/sass/*.scss'],
+        tasks: ['sass', 'cssmin'],
+        options: {
+          spawn: false
+        }
       }
-    }
+    },
   });
 
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-sass');
   grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-imagemin');
+  grunt.loadNpmTasks('grunt-svgmin');
   grunt.loadNpmTasks('grunt-contrib-watch');
 
-  grunt.registerTask('default', ['concat', 'uglify', 'sass', 'cssmin']);
+  grunt.registerTask('default', ['concat', 'uglify', 'sass', 'cssmin', 'imagemin', 'svgmin']);
+
+  grunt.registerTask('dev', ['watch']);
+
 };
