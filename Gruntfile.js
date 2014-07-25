7494c3cb2301e38c749db5cb7533aeb333ae2768
kaspar-naaber <kaspar.naaber@outlook.com>
2014-07-25 10:56:54 +0300

    Add and update configuration files

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,25 +1,80 @@
 module.exports = function(grunt) {
+  grunt.loadNpmTasks('grunt-bowercopy');
+  grunt.loadNpmTasks('grunt-modernizr');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-imagemin');
+  grunt.loadNpmTasks('grunt-svgmin');
+  grunt.loadNpmTasks('grunt-newer');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
 
+    // Copys the source files from the bower directory to the project's source locations.
+    bowercopy: {
+      options: {
+        srcPrefix: 'bower_components'
+      },
+
+      javascripts: {
+        options: {
+          destPrefix: 'javascripts/src/'
+        },
+        files: {
+          'concat/jquery.js': 'jquery/dist/jquery.js',
+          'concat/overthrow.js': 'overthrow/src/overthrow-polyfill.js',
+          'modernizr.js': 'modernizr/modernizr.js'
+        }
+      },
+
+      stylesheets: {
+        options: {
+          destPrefix: 'stylesheets/scss/'
+        },
+        files: {
+          'bourbon': 'bourbon/dist'
+        }
+      }
+    },
+
+    // Builds custom modernizr script.
+    modernizr: {
+      build: {
+        'devFile' : 'javascripts/src/modernizr.js',
+        'outputFile' : 'javascripts/modernizr.js',
+
+        'tests': [
+          'flexbox',
+          'svg'
+        ],
+
+        'uglify' : false
+      }
+    },
+
+    // Concatenates javascripts into one file.
     concat: {
       build: {
         src: [
-        'javascripts/src/jquery.js',
-        'javascripts/src/*.js'
+        'javascripts/src/concat/jquery.js',
+        'javascripts/src/concat/*.js'
         ],
         dest: 'javascripts/application.js'
       }
     },
 
+    // Minifies the javascript files.
     uglify: {
       build: {
         files: [{
           expand: true,
           cwd: 'javascripts/',
           src: [
-          '*.js',
-          '!*.min.js'
+            '*.js',
+            '!*.min.js'
           ],
           dest: 'javascripts/',
           ext: '.min.js'
@@ -27,6 +82,7 @@ module.exports = function(grunt) {
       }
     },
 
+    // Compiles the stylesheet files.
     sass: {
       build: {
         options: {
@@ -34,19 +90,23 @@ module.exports = function(grunt) {
         },
         files: [{
           expand: true,
-          cwd: 'stylesheets/sass',
-          src: ['*.scss'],
+          cwd: 'stylesheets/scss',
+          src: '*.scss',
           dest: 'stylesheets',
           ext: '.css'
         }]
       }
     },
 
+    // Minifies the stylesheet files.
     cssmin: {
       build: {
         expand: true,
         cwd: 'stylesheets/',
-        src: ['*.css', '!*.min.css'],
+        src: [
+          '*.css',
+          '!*.min.css'
+        ],
         dest: 'stylesheets/',
         ext: '.min.css',
         options: {
@@ -55,46 +115,71 @@ module.exports = function(grunt) {
       }
     },
 
+    // Minifies the image files.
     imagemin: {
       images: {
         files: [{
           expand: true,
-          cwd: 'images/src',
-          src: ['*.{png,jpg,gif}',],
+          cwd: 'images/src/',
+          src: '*.{png,jpg,gif}',
           dest: 'images/'
         }]
-      },
+      }
     },
 
+    // Minifies the scalable vector graphics files
     svgmin: {
       build: {
         files: [{
           expand: true,
-          cwd: 'assets/src',
-          src: ['*.svg'],
+          cwd: 'assets/src/',
+          src: '*.svg',
           dest: 'assets/',
           ext: '.svg'
         }]
       },
     },
 
+    // Watches the project for changes and recompiles the output files.
     watch: {
       concat: {
-        files: ['javascripts/src/*.js'],
-        tasks: 'concat'
+        files: 'javascripts/src/concat/*.js',
+        tasks: 'concat',
+        options: {
+          spawn: false
+        }
       },
 
       uglify: {
-        files: ['javascripts/*.js', '!javascripts/*.min.js'],
-        tasks: 'uglify',
+        files: [
+        'javascripts/*.js',
+        '!javascripts/*.min.js'
+        ],
+        tasks: 'newer:uglify',
         options: {
           spawn: false
         }
       },
 
       css: {
-        files: ['stylesheets/sass/*.scss'],
-        tasks: ['sass', 'cssmin'],
+        files: 'stylesheets/scss/*.scss',
+        tasks: ['sass', 'newer:cssmin'],
+        options: {
+          spawn: false
+        }
+      },
+
+      imagemin:  {
+        files: 'images/src/*.{png,jpg,gif}',
+        tasks: 'newer:imagemin',
+        options: {
+          spawn: false
+        }
+      },
+
+      svgmin: {
+        files: 'assets/src/*.svg',
+        tasks: 'newer:svgmin',
         options: {
           spawn: false
         }
@@ -102,16 +187,5 @@ module.exports = function(grunt) {
     },
   });
 
-  grunt.loadNpmTasks('grunt-contrib-concat');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-sass');
-  grunt.loadNpmTasks('grunt-contrib-cssmin');
-  grunt.loadNpmTasks('grunt-contrib-imagemin');
-  grunt.loadNpmTasks('grunt-svgmin');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-
-  grunt.registerTask('default', ['concat', 'uglify', 'sass', 'cssmin', 'imagemin', 'svgmin']);
-
-  grunt.registerTask('dev', ['watch']);
-
+  grunt.registerTask('default', ['bowercopy', 'modernizr', 'concat', 'uglify', 'sass', 'cssmin', 'imagemin', 'svgmin']);
 };
