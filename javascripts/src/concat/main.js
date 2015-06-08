c721a811d803722a7fb56cb686be79d4b3200bf6
Kaspar Naaber <kaspar.naaber@outlook.com>
2015-06-08 14:09:42 +0300

    Remove deprecated functions and gallery hover effect

--- javascripts/src/concat/main.js
+++ javascripts/src/concat/main.js
@@ -72,17 +72,6 @@
     });
   };
 
-  // Reduces opacity of the gallery images that are not under the cursor.
-  var handleGalleryHover = function() {
-    $('.edys-gallery-item').mouseover(function() {
-      $(this).siblings('.edys-gallery-item').find('.edys-gallery-image').addClass('inactive');
-    });
-
-    $('.edys-gallery-item').mouseout(function() {
-      $(this).siblings('.edys-gallery-item').find('.edys-gallery-image').removeClass('inactive');
-    });
-  };
-
   // Scrolls to the comment-form if comment submit failed (to show the error messages to the user)
   var focusFormWithErrors = function() {
     $(document).ready(function() {
@@ -98,29 +87,6 @@
   // TODO: remove if edicy is going to wrap table with the container.
   var wrapTables = function() {
     $('.content-formatted table').wrap('<div class="table-container overthrow"></div>');
-  };
-
-  // Checks the presence of the table scrollbar.
-  var checkScrollBar = function() {
-    jQuery.fn.hasScrollBar = function(direction) {
-      if (direction == 'vertical') {
-        return this.get(0).scrollHeight > this.innerHeight();
-      } else if (direction == 'horizontal') {
-        return this.get(0).scrollWidth > this.innerWidth();
-      }
-      return false;
-    }
-  };
-
-  // Adds horizontal scroll to tables that don't fit into the content area.
-  var handleTableHorizontalScrolling = function() {
-    $.each($('.table-container'), function() {
-      if ($(this).hasScrollBar('horizontal') === true) {
-        $(this).addClass('horizontal-scroll');
-      } else {
-        $(this).removeClass('horizontal-scroll');
-      }
-    });
   };
 
   // Initiates the functions when window is resized.
@@ -141,14 +107,9 @@
   var init = function() {
     handleElementsClick();
     handleSearchSubmit();
-    handleGalleryHover();
     handleWindowResize();
     focusFormWithErrors();
     wrapTables();
-    if ($('.table-container').length > 0) {
-      checkScrollBar();
-      handleTableHorizontalScrolling();
-    }
   };
 
   window.site = $.extend(window.site || {}, {
