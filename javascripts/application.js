98c0a1799f77b45c31685a84400ee303ff8b2251
Kaspar Naaber <kaspar.naaber@outlook.com>
2017-03-06 14:19:21 +0200

    Replace JS hack with proper button for article adding modal.

--- javascripts/application.js
+++ javascripts/application.js
@@ -23809,10 +23809,6 @@ MMCQ = (function() {
     $('.js-search').click(function(event) {
       // event.stopPropagation();
     });
-
-    $('.js-post-add').click(function() {
-      $('.js-post-add-link-button-wrapper').find('.edy-add-modal').trigger('click');
-    });
   };
 
   // Sets the search modal synamic height.
