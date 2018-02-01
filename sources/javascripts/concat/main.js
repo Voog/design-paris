ec52d75da2d6d82875d336b3869d0ea348d8c433
Siim Häelm <siimhaelm@gmail.com>
2018-02-01 16:38:29 +0200

    "Search not found" translation to current viewing language.

--- sources/javascripts/concat/main.js
+++ sources/javascripts/concat/main.js
@@ -356,6 +356,30 @@
     };
   };
 
+  // ===========================================================================
+  // Binds site search functionality.
+  // ===========================================================================
+  var bindSiteSearch = function(searchForm, languageCode, noResultsString) {
+    if (searchForm) {
+      var search = new VoogSearch(searchForm, {
+        // This defines the number of results per query.
+        per_page: 10,
+        // Language code for restricting the search to page language.
+        lang: languageCode,
+        // If given, an DOM element results are rendered inside that element
+        resultsContainer: $('.js-voog-search-modal-inner').get(0),
+        // Defines if modal should close on sideclick.
+        sideclick: true,
+        // Mobile checkpoint.
+        mobileModeWidth: 640,
+        // Updates results on every keypress.
+        updateOnKeypress: true,
+        // String for feedback if no results are found.
+        noResults: noResultsString
+      });
+    } 
+  };
+
   window.site = $.extend(window.site || {}, {
     initBlogPage: initBlogPage,
     initArticlePage: initArticlePage,
@@ -363,7 +387,8 @@
     bodyBgCommit: bodyBgCommit,
     debounce: debounce,
     getMoreArticles: getMoreArticles,
-    bindCustomTexteditorStyles: bindCustomTexteditorStyles
+    bindCustomTexteditorStyles: bindCustomTexteditorStyles,
+    bindSiteSearch: bindSiteSearch
   });
 
   init();
